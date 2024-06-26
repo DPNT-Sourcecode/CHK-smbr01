50cf72cfdb8b60e7ab03054dca4d769f982eb100
sbx_user1051 <sbx_user1051@169.254.108.253>
2024-06-26 21:45:24 +0000

    Wed Jun 26 21:45:24 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -44,23 +44,21 @@ def _calculate_total_price(products_in_basket_sku_list):
     # check special offers
     number_of_a_discounts = int(number_of_each_item['A'] / 3)
     potential_number_of_free_b_products = int(number_of_each_item['E'] / 2)
-    # actual_number_of_free_b_products = potential_number_of_free_b_products if potential_number_of_free_b_products > 0 else 0
 
-    # if actual_number_of_free_b_products:
     temp_b_count = number_of_each_item['B']
     temp_b_count -= potential_number_of_free_b_products
 
     if temp_b_count < 0:
         potential_number_of_free_b_products = 0
 
-    # number_of_b_discounts = int(number_of_each_item['B'] / 2)
     number_of_b_discounts = int(temp_b_count / 2)
 
     basket_sub_total = sum([PRICE_LIST[sku]
                            for sku in products_in_basket_sku_list])
 
     basket_total_post_discounts = basket_sub_total - \
-        (number_of_a_discounts * 20) - (number_of_b_discounts * 15) - (potential_number_of_free_b_products * 30)
+        (number_of_a_discounts * 20) - (number_of_b_discounts * 15) - \
+        (potential_number_of_free_b_products * 30)
 
     return basket_total_post_discounts
 
@@ -75,16 +73,12 @@ def checkout(skus: str) -> int:
     products_in_basket_sku_list = list(skus)
 
     basket_valid = _is_basket_valid(products_in_basket_sku_list)
-    # TODO fallback plan if not comma seperated
-    # countA = skus.count("A")
-    # countB = skus.count("B")
-    # countC = skus.count("C")
-    # countD = skus.count("D")
 
     if basket_valid:
         return _calculate_total_price(products_in_basket_sku_list)
     else:
         return -1
+
 
 
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -20,43 +20,54 @@ import pytest
 
 
 class TestChk():
+    def test_checkout_empty_basket(self):
+        expected_total_price = 0
+        total_price = checkout_solution.checkout("")
+        assert total_price == expected_total_price
 
-    # def test_checkout_empty_basket(self):
-    #     expected_total_price = 0
-    #     total_price = checkout_solution.checkout("")
-    #     assert total_price == expected_total_price
+    def test_checkout_simple_basket(self):
+        expected_total_price = 115
+        total_price = checkout_solution.checkout("ABCD")
+        assert total_price == expected_total_price
 
-    # def test_checkout_simple_basket(self):
-    #     expected_total_price = 115
-    #     total_price = checkout_solution.checkout("ABCD")
-    #     assert total_price == expected_total_price
+    def test_checkout_extra_item_basket(self):
+        expected_total_price = 130
+        total_price = checkout_solution.checkout("ABCDD")
+        assert total_price == expected_total_price
 
-    # def test_checkout_extra_item_basket(self):
-    #     expected_total_price = 130
-    #     total_price = checkout_solution.checkout("ABCDD")
-    #     assert total_price == expected_total_price
+    def test_checkout_invalid_basket(self):
+        expected_response = -1
+        response = checkout_solution.checkout("AB@%@£%CD")
+        assert response == expected_response
 
-    # def test_checkout_invalid_basket(self):
-    #     expected_response = -1
-    #     response = checkout_solution.checkout("AB@%@£%CD")
-    #     assert response == expected_response
+    def test_checkout_special_offer_a(self):
+        expected_total_price = 130
+        total_price = checkout_solution.checkout("AAA")
+        assert total_price == expected_total_price
 
-    # def test_checkout_special_offer_a(self):
-    #     expected_total_price = 130
-    #     total_price = checkout_solution.checkout("AAA")
-    #     assert total_price == expected_total_price
-
-    # def test_checkout_special_offer_b(self):
-    #     expected_total_price = 75
-    #     total_price = checkout_solution.checkout("BBB")
-    #     assert total_price == expected_total_price
+    def test_checkout_special_offer_b(self):
+        expected_total_price = 75
+        total_price = checkout_solution.checkout("BBB")
+        assert total_price == expected_total_price
     
     def test_checkout_special_offer_3e_for_b(self):
-        # expected_total_price_before_discounts = 40+40+40+40+30+30
-        # expected_total_price_before_discounts = 40+40+40+40 - the free Bs removed
+        # the free Bs removed
         expected_total_price = 40+40+40+40
         total_price = checkout_solution.checkout("EEEEBB")
         assert total_price == expected_total_price
+
+    def test_checkout_special_offer_3e_for_b_extra_b(self):
+        # the free Bs removed
+        expected_total_price = 40+40+40+40+30
+        total_price = checkout_solution.checkout("EEEEBBB")
+        assert total_price == expected_total_price
+    
+    def test_checkout_special_offer_3e_for_b_extra_2bs(self):
+        # the free Bs removed
+        expected_total_price = 40+40+40+40+45
+        total_price = checkout_solution.checkout("EEEEBBBB")
+        assert total_price == expected_total_price
+
 
 
 
