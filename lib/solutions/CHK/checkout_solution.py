b1b0ee4088a2f0021e97504c99f29c36662c8f29
sbx_user1051 <sbx_user1051@169.254.108.253>
2024-06-26 23:25:08 +0000

    Wed Jun 26 23:25:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -118,8 +118,9 @@ def _discount_parser(original_price_per_unit: int, discount_rule: str) -> dict:
     }
 
 
-def _per_row_discount_evaluator(row, discount_info):
+def _per_row_discount_evaluator(number_of_each_item_lookup, index, row, discount_info):
     # check how many times triggered
+    number_of_each_item_lookup[index]
     # calculate total discount
     # add it to the target row
 
@@ -134,16 +135,16 @@ def _per_row_discount_evaluator(row, discount_info):
 
 
 def _calculate_total_price(products_in_basket_sku_list):
-    number_of_each_item = {sku: 0 for sku in PRICE_LIST}
+    number_of_each_item_lookup = {sku: 0 for sku in PRICE_LIST}
     for sku in products_in_basket_sku_list:
-        number_of_each_item[sku] += 1
+        number_of_each_item_lookup[sku] += 1
 
     # check special offers
-    # number_of_5a_discounts, a_remainder = divmod(number_of_each_item['A'], 5)
+    # number_of_5a_discounts, a_remainder = divmod(number_of_each_item_lookup['A'], 5)
     # number_of_3a_discounts = int(a_remainder / 3)
-    # potential_number_of_free_b_products = int(number_of_each_item['E'] / 2)
+    # potential_number_of_free_b_products = int(number_of_each_item_lookup['E'] / 2)
 
-    # temp_b_count = number_of_each_item['B']
+    # temp_b_count = number_of_each_item_lookup['B']
     # temp_b_count -= potential_number_of_free_b_products
 
     # if temp_b_count < 0:
@@ -151,12 +152,12 @@ def _calculate_total_price(products_in_basket_sku_list):
 
     # number_of_b_discounts = int(temp_b_count / 2)
 
-    # number_of_f_free = int(number_of_each_item['F'] / 3)
+    # number_of_f_free = int(number_of_each_item_lookup['F'] / 3)
 
     # manage discounts
     for index, row in price_df.iterrows():
         discount_info = _discount_parser(row["price"], row["discount_rule"])
-        _per_row_discount_evaluator(row, discount_info)
+        _per_row_discount_evaluator(number_of_each_item_lookup, index, row, discount_info)
         breakpoint()
 
     basket_sub_total = sum([PRICE_LIST[sku]
@@ -185,6 +186,7 @@ def checkout(skus: str) -> int:
         return _calculate_total_price(products_in_basket_sku_list)
     else:
         return -1
+
 
 
 
