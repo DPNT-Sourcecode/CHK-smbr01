3d13d486e193f732c1631a4f7f93c159708cff7b
sbx_user1051 <sbx_user1051@169.254.108.253>
2024-06-26 20:50:26 +0000

    Wed Jun 26 20:50:26 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -33,9 +33,10 @@ def _is_basket_valid(products_in_basket_sku_list):
     return True
 
 def  _calculate_total_price(products_in_basket_sku_list):
-    number_of_each_item = {k,v for k,v in PRICE_LIST.items()}
+    number_of_each_item = {sku: 0 for sku in PRICE_LIST}
     for sku in products_in_basket_sku_list:
-        number_of_each_item
+        number_of_each_item[sku] += 1
+    breakpoint()
     return sum([PRICE_LIST[sku] for sku in products_in_basket_sku_list])
 
 def checkout(skus: str) -> int:
@@ -55,6 +56,7 @@ def checkout(skus: str) -> int:
         return _calculate_total_price(products_in_basket_sku_list)
     else:
         return -1
+
 
 
 
