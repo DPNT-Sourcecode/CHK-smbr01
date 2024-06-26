a95ea6fc91949fcd5258aedfd36943b24bc44928
sbx_user1051 <sbx_user1051@169.254.108.253>
2024-06-26 20:02:25 +0000

    Wed Jun 26 20:02:25 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,9 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    # TODO
+    # total price of a number of items
+    # things are identified using Stock Keeping Units, or SKUs
+    # some items are multi-priced: buy n of them, and they'll cost you y pounds
     raise NotImplementedError()
+
