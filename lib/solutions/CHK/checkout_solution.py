84b19967a01ae9a85e7f3d14abaede003862fb54
sbx_user1051 <sbx_user1051@169.254.108.253>
2024-06-27 01:40:08 +0000

    Thu Jun 27 01:40:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -71,32 +71,32 @@ basket_discount_tracker = {
 }
 # sorted by BOGOF first (NOTE a python version should be used which retains dictionary order)
 ITEM_PRICE_DISCOUNT_LOOKUP = {
-    # "E": [40, "2E get one B free"],
+    "E": [40, "2E get one B free"],
     "F": [10, "2F get one F free"],
-    # "N": [40, "3N get one M free"],
-    # "R": [50, "3R get one Q free"],
-    # "U": [40, "3U get one U free"],
-    # "A": [50, "3A for 130, 5A for 200"],
-    # "B": [30, "2B for 45"],
-    # "C": [20, ""],
-    # "D": [15, ""],
-    # "G": [20, ""],
-    # "H": [10, "5H for 45, 10H for 80"],
-    # "I": [35, ""],
-    # "J": [60, ""],
-    # "K": [80, "2K for 150"],
-    # "L": [90, ""],
-    # "M": [15, ""],
-    # "O": [10, ""],
-    # "P": [50, "5P for 200"],
-    # "Q": [30, "3Q for 80"],
-    # "S": [30, ""],
-    # "T": [20, ""],
-    # "V": [50, "2V for 90, 3V for 130"],
-    # "W": [20, ""],
-    # "X": [90, ""],
-    # "Y": [10, ""],
-    # "Z": [50, ""],
+    "N": [40, "3N get one M free"],
+    "R": [50, "3R get one Q free"],
+    "U": [40, "3U get one U free"],
+    "A": [50, "3A for 130, 5A for 200"],
+    "B": [30, "2B for 45"],
+    "C": [20, ""],
+    "D": [15, ""],
+    "G": [20, ""],
+    "H": [10, "5H for 45, 10H for 80"],
+    "I": [35, ""],
+    "J": [60, ""],
+    "K": [80, "2K for 150"],
+    "L": [90, ""],
+    "M": [15, ""],
+    "O": [10, ""],
+    "P": [50, "5P for 200"],
+    "Q": [30, "3Q for 80"],
+    "S": [30, ""],
+    "T": [20, ""],
+    "V": [50, "2V for 90, 3V for 130"],
+    "W": [20, ""],
+    "X": [90, ""],
+    "Y": [10, ""],
+    "Z": [50, ""],
 }
 
 # set up dataframe for item price tabular data
@@ -176,8 +176,8 @@ def _bogof_evaluator(basket_contents_lookup, index, row, discount_info):
     number_of_discounts_triggered, remainder = divmod(number_of_this_item_in_basket, discount_info['number_of_items_required_to_trigger'])
 
     # If the free product isn't in the basket, we won't award the discount
-    if number_of_discounts_triggered and discount_info['discount_target_sku'] == index:
-        import pdb;pdb.set_trace()
+    if discount_info['discount_target_sku'] == index and number_of_discounts_triggered and not remainder:
+        number_of_discounts_triggered  -= 1
 
     # add it to the target row
     total_discount_for_rule = discount_info['discount_per_trigger'] * number_of_discounts_triggered
@@ -233,11 +233,3 @@ def checkout(skus: str) -> int:
         return _calculate_total_price(products_in_basket_sku_list)
     else:
         return -1
-
-
-
-
-
-
-
-
